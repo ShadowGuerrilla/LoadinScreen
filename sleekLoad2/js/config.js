b954b549a5245dfebd0a03dfd0afed077c12cd9f
ShadowGuerrilla <42214687+ShadowGuerrilla@users.noreply.github.com>
2018-10-13 22:55:44 -0400

    Update config.js

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "The Cook Network";
+var l_serverName = "Guerrilla Gaming CWRP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
